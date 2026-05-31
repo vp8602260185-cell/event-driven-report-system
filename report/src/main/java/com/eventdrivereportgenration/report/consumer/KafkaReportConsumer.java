3c027d5944ab3b2de7f8b096e838ccd0cd107485
Vishal Patel <Vp8602260185@gmail.com>
2026-05-31 23:28:24 +0530

    Implement DLQ with retry

--- report/src/main/java/com/eventdrivereportgenration/report/consumer/KafkaReportConsumer.java
+++ report/src/main/java/com/eventdrivereportgenration/report/consumer/KafkaReportConsumer.java
@@ -1,7 +1,10 @@
 package com.eventdrivereportgenration.report.consumer;
 
 import com.eventdrivereportgenration.report.eventdto.ReportGeneratedEvent;
+
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +14,20 @@ public class KafkaReportConsumer {
             topics = "report.generated",
             groupId = "report-group"
     )
+    @RetryableTopic(attempts = "5")
     public void consume1(ReportGeneratedEvent event) {
         System.out.println("Modify report: " + event.getReportId());
+        throw new RuntimeException("Simulated failure in consume1");
+    }
+
+    @DltHandler
+    public void handleDlt(ReportGeneratedEvent event) {
+
+        System.out.println("DLT Received : " + event.getReportId());
+
+        // Save to DB
+        // Send alert
+        // Create incident
     }
 
 }
